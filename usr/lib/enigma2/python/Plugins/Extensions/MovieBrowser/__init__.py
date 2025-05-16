609f3e009558963de9c5cee0887ccfeeb4170720
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-05-16 23:33:34 +0200

    Movie Browser 3.9-rc0

--- usr/lib/enigma2/python/Plugins/Extensions/MovieBrowser/__init__.py
+++ usr/lib/enigma2/python/Plugins/Extensions/MovieBrowser/__init__.py
@@ -1,38 +1,41 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from os import environ
+from os.path import exists
+from sys import version_info
+import gettext
 
 from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS
-import gettext
-import os
 
+PluginLanguageDomain = "moviebrowser"
+PluginLanguagePath = "Extensions/MovieBrowser/locale"
 
-PluginLanguageDomain = 'moviebrowser'
-PluginLanguagePath = 'Extensions/MovieBrowser/locale'
-
-isDreambox = False
-if os.path.exists("/usr/bin/apt-get"):
-    isDreambox = True
+isDreambox = exists("/usr/bin/apt-get")
+PY3 = version_info.major >= 3
 
 
 def localeInit():
-    if isDreambox:
-        lang = language.getLanguage()[:2]
-        os.environ["LANGUAGE"] = lang
-    gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
+	if isDreambox:
+		lang = language.getLanguage()[:2]
+		environ["LANGUAGE"] = lang
+	if PluginLanguageDomain and PluginLanguagePath:
+		gettext.bindtextdomain(PluginLanguageDomain, resolveFilename(SCOPE_PLUGINS, PluginLanguagePath))
 
 
 if isDreambox:
-    def _(txt):
-        return gettext.dgettext(PluginLanguageDomain, txt) if txt else ""
+	def _(txt):
+		return gettext.dgettext(PluginLanguageDomain, txt) if txt else ""
 else:
-    def _(txt):
-        translated = gettext.dgettext(PluginLanguageDomain, txt)
-        if translated:
-            return translated
-        else:
-            print(("[%s] fallback to default translation for %s" % (PluginLanguageDomain, txt)))
-            return gettext.gettext(txt)
+	def _(txt):
+		translated = gettext.dgettext(PluginLanguageDomain, txt)
+		if translated:
+			return translated
+		else:
+			print("[%s] fallback to default translation for %s" % (PluginLanguageDomain, txt))
+			return gettext.gettext(txt)
 
 localeInit()
 language.addCallback(localeInit)

93d939a824259bb3de50c9dea5c78477f832a078
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2026-04-04 19:58:45 +0200

    Update plugin.py

--- usr/lib/enigma2/python/Plugins/Extensions/MovieBrowser/plugin.py
+++ usr/lib/enigma2/python/Plugins/Extensions/MovieBrowser/plugin.py
@@ -25,6 +25,10 @@ from shutil import copytree, copy2, rmtree
 from requests import get
 from urllib.error import HTTPError, URLError
 from twisted.internet.reactor import callInThread
+    from urllib.request import urlretrievee
+except ImportError:  # Python 2
+    from urllib import urlretrieve
+
 
 from enigma import (
     RT_HALIGN_LEFT,
